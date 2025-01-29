4f878832b0ae5c79ceed385ecc2735d9700ee13d
florinpfm1 <florinfmp@gmail.com>
2025-01-29 11:40:30 +0200

    updates

--- CSharpAdvancedTopics/Section2_LINQ/ConsoleApp1-LINQ/ConsoleApp1-LINQ/Program.cs
+++ CSharpAdvancedTopics/Section2_LINQ/ConsoleApp1-LINQ/ConsoleApp1-LINQ/Program.cs
@@ -4,11 +4,12 @@
     {
         static void Main(string[] args)
         {
+            // LINQ queries applied on array of integer and strings
             string sentence = "I love cats";
             string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
             int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
 
-            // loop through, apply condition, when condition is met add item to new list
+            // loop through, apply condition, when condition is met add item to new list  <<<=== ALL THIS CAN BE DONE WITH LINQ !! ***
             List<int> newNumbersBelowFive = new List<int>();
             foreach (var number in numbers)
             {
@@ -21,20 +22,123 @@
 
             // all the above can be executed the same with fewer code using LINQ queries
             var getTheNumbersBelowFive = from number in numbers
-                                where number < 5
-                                select number;
+                                         where number < 5
+                                         select number;
             Console.WriteLine(string.Join(", ", getTheNumbersBelowFive));  //<<-- the LINQ query is executed only in this line where
-                                                                  //the var getTheNumbers is actually used for something
+                                                                           //the var getTheNumbers is actually used for something
 
-            // 
-
-
-
-
-
-
+            var getTheNumbersBelowFive2 = from number in numbers
+                                          where number > 5 && number < 10 // we can have multiple filtering conditions
+                                          orderby number
+                                          select number;
+            Console.WriteLine(string.Join(", ", newNumbersBelowFive));
 
 
+            // to filter cat names containing letter "a" and length greater than 4
+            var catsWithA = from cat in catNames
+                            where cat.Contains("a") && (cat.Length > 4)
+                            orderby cat // by default sorts "ascending", no need to use ascending here
+                            select cat;
+            Console.WriteLine(string.Join(", ", catsWithA));
+
+            var catsWithA2 = from cat in catNames
+                             where cat.Contains("a")  // we can have multiple filtering conditions
+                             where cat.Length > 4     // we can have multiple filtering conditions
+                             orderby cat descending
+                             select cat;
+            Console.WriteLine(string.Join(", ", catsWithA2));
+
+            // LINQ queries applied on objects
+            List<Person> people = new List<Person>
+            {
+                new Person("Tod", 180, 70, Gender.Male),
+                new Person("John", 170, 88, Gender.Male),
+                new Person("Anna", 150, 48, Gender.Female),
+                new Person("Kyle", 164, 77, Gender.Male),
+                new Person("Anna", 164, 77, Gender.Male),
+                new Person("Maria", 160, 55, Gender.Female),
+                new Person("John", 160, 55, Gender.Female)
+            };
+
+            var fourCharPeople = from p in people
+                                 where (p.Name.Length == 4)
+                                 orderby p.Weight
+                                 select p;
+            foreach (var item in fourCharPeople)
+            {
+                Console.WriteLine($"Name: {item.Name}, Weight: {item.Weight}");
+            }
+
+            var fourCharPeople2 = from p in people
+                                 where (p.Name.Length == 4)
+                                 orderby p.Name
+                                 select p.Name;
+            foreach (var item in fourCharPeople2)
+            {
+                Console.WriteLine($"Name: {item}");
+            }
+
+            var fourCharPeople3 = from p in people
+                                 where (p.Name.Length == 4)
+                                 orderby p.Name descending, p.Height // <<<--- we can have multiple orderby conditions
+                                                          // first orders by Name, then all with the same name are ordered by Height
+                                  select p;
+            foreach (var item in fourCharPeople3)
+            {
+                Console.WriteLine($"Name: {item.Name}, Height: {item.Height}");
+            }
+
+
+
+
+        }
+
+        internal class Person
+        {
+            //fields
+            private string name;
+            private int height;
+            private int weight;
+            private Gender gender;
+
+            //props
+            public string Name
+            {
+                get { return this.name; }
+                set { this.name = value; }
+            }
+
+            public int Height
+            {
+                get { return this.height; }
+                set { this.height = value; }
+            }
+            public int Weight
+            {
+                get { return this.weight; }
+                set { this.weight = value; }
+            }
+
+            public Gender Gender
+            {
+                get;
+                set;
+            }
+
+            //CTOR
+            public Person(string name, int height, int weight, Gender gender)
+            {
+                this.Name = name;
+                this.Height = height;
+                this.Weight = weight;
+                this.Gender = gender;
+            }
+        }
+
+        internal enum Gender
+        {
+            Male,
+            Female
         }
     }
 }
---
+++ CSharpAdvancedTopics/Section3_LINQwithLAMBDAEXPRESSIONS/ConsoleApp1_LINQ_with_LambdaExpressions/ConsoleApp1_LINQ_with_LambdaExpressions/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Linq;
+
+namespace ConsoleApp1_LINQ_with_LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+            List<int> numbers = new List<int>() { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
+            object[] mix = { 1, "string", 'd', new List<int>() { 1, 2, 3, 4 }, new List<int>() { 5, 2, 3, 4 }, "dd", 's', "Hello Kitty", 1, 2, 3, 4, };
+
+            // find the odd numbers through LINQ query
+            var oddNumbers = from n in numbers
+                             where n % 2 == 1
+                             select n;
+            Console.WriteLine(string.Join(", ", oddNumbers));
+
+            // same as above but with LINQ with Lambda Expression
+            var oddNumbers2 = numbers.Where(n => n % 2 == 1);
+            /*
+             n is the iterating variable ; => is the lambda operator ; numbers is the collection through which we will iterate ; n % 2 == 1 is the condition
+             if the condition is true, the number will be added to the oddNumbers2 collection
+            */
+            Console.WriteLine(string.Join(", ", oddNumbers2));
+
+            // if we want to convert the var oddNumbers2 to a List<int> type, then we need to add .ToList() at the end of the statement
+            List<int> oddNumbers3 = numbers.Where(n => n % 2 == 1).ToList();
+            Console.WriteLine(string.Join(", ", oddNumbers3));
+
+            // check what is the average char length of the items in cat names list
+            double average = catNames.Average(c => c.Length);
+            Console.WriteLine(average);
+            double minCatNamesLength = catNames.Min(c => c.Length);
+            Console.WriteLine(minCatNamesLength);
+            double maxCatNamesLength = catNames.Max(c => c.Length);
+            Console.WriteLine(maxCatNamesLength);
+            double sumCatNamesLength = catNames.Sum(c => c.Length);
+            Console.WriteLine(sumCatNamesLength);
+
+            // extract all integers from the mix array of objects (some of them are integers which we want to extract)
+            var allIntegrs = mix.OfType<int>();
+            Console.WriteLine(string.Join(", ", allIntegrs));
+
+            var allIntegrs2 = mix.OfType<int>().Where(i => i < 3);
+            Console.WriteLine(string.Join(", ", allIntegrs2));
+
+            var allIntLists = mix.OfType<List<int>>().ToList();
+            for (int i = 0; i < allIntLists.Count(); i++)
+            {
+                Console.WriteLine($"Int of List[{i}]: " + string.Join(", ", allIntLists[i]));
+            }
+
+            // e.g. how to extract a list of derived classes Melle and Spellcaster from a list of base class Characters:
+            //      basically we are extracting a derived type from a base type
+
+            //List<Character> characters = new List<Character>(); //in this list we have some Melle and some Spellcaster objects
+            //List<Melle> melleTeam = new List<Melle>();
+            //List<Spellcaster> spellcasterTeam = new List<Spellcaster>();
+
+            //1st way to do this
+            //melleTeam = characters.OfType<Melee>.ToList();
+
+            //2nd way to do this with Lambda Expression
+            //melleTeam = characters.Where(c => c is Melee).Cast<Melle>.ToList();
+
+            //speallcasterTeam = characters.Where(c => c is Spellcaster).Cast<Spellcaster>.ToList();
+
+
+            //Difference between "Select" and "Where": Select is used to project data, Where is used to filter data
+            List<Warrior> warriors = new List<Warrior>()
+            {
+                new Warrior() { Height = 100 },
+                new Warrior() { Height = 80 },
+                new Warrior() { Height = 100 },
+                new Warrior() { Height = 70 },
+            };
+
+            var shortWarriors = warriors.Where(wh => (wh.Height == 100)); //"Where" returns a collection of Warrior objects
+            var shortWarriors2 = warriors.Where(wh => (wh.Height == 100))
+                                         .Select(wh => (wh.Height)); //"Select" returns a collection from another collection
+                                                                     //from Warriro collection will make a new collections of int Height of each Warrior
+
+            // a feature that only the List type have: a built-in foreach method :):):) which work like a foreach loop
+            List<int> shortWarriors3 = warriors.Where(wh => (wh.Height == 100))
+                                               .Select(wh => (wh.Height))
+                                               .ToList();
+            //we want to print in console the height of all our warrior objects
+            Console.WriteLine(string.Join(", ", warriors)); //<<-- this will give some error in the console, because we are trying to print a list of objects and it cannot convert from an object to a string :(
+
+            warriors.ForEach(w => Console.WriteLine(w.Height)); //<<-- this will work, because we are using the ForEach method of the List type, which is a built-in foreach loop and we print directly the Height of each Warrior object
+            warriors.ForEach(w => Console.Write(w.Height + " "));
+            Console.WriteLine();
+            shortWarriors3.ForEach(sw => Console.WriteLine(sw));
+
+            //note that this:     warriors.ForEach(w => Console.WriteLine(w.Height));    is the same as this:
+            foreach (var w in warriors)
+            {
+                Console.WriteLine(w.Height);
+            }
+        }
+
+        internal class Warrior
+        {
+            public int Height { get; set; }
+        }
+
+        private static int[] StringToIntArray(string array)
+        {
+            int[] arrayFromString = array.Split(' ')
+                                          .Select(element => int.Parse(element))
+                                          .ToArray();   
+            return arrayFromString;
+        }
+    }
+}
